b6fa7c11e69d11eb6b96d3f0fed660acb1fda50d
Prousika P N <157342963+Prousika@users.noreply.github.com>
2026-07-03 14:41:12 +0530

    Add files via upload

---
+++ DeepSkilling/JUnit, Mockito and SL4J/SL4J Logging exercises/Exercise1-Logging/src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main{
+
+    private static final Logger logger =
+            LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args){
+        logger.info("Application Started");
+        logger.warn("This is a warning message");
+        logger.error("This is an error message");
+    }
+
+}
