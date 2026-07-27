82bb510feecd63f1d549170408fb63b1a4f36594
Prousika P N <157342963+Prousika@users.noreply.github.com>
2026-07-27 16:18:00 +0530

    exercise-1

---
+++ DeepSkilling/Microservices/Exercise2__Creating Microservices for account and loan/loan/src/test/java/org/cognizant/loan/LoanApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cognizant.loan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
